8a57c38499f90479affb26db7067b8a708ed078a
Oleksandra Baga <oleksandra.baga@gmail.com>
2020-11-29 21:30:56 +0100

    client-server dns request-response found record in dns_db and send back ip-address to a client. TODO: recursive search for ip records when more servers are started

--- src/authorative_server.cpp
+++ src/authorative_server.cpp
@@ -5,6 +5,7 @@
 
 #include "udp.hpp"
 #include "dns_db.h"
+#include "authorative_server.h"
 
 //#define SERVER_IP 	"127.0.0.10"
 #define MAXCHAR 100
@@ -24,7 +25,7 @@ bool read_config(const char *server_name, DnsDb *db) {
     }
     char s1[20], s2[20];
     while (fgets(str, MAXCHAR, fp) != NULL) {
-        printf("%s", str);
+        //printf("%s", str);
         sscanf(str, "%s %s", s1, s2);
         //printf("GOT: %s: %s\n", s1, s2);
         DnsRecord record;
@@ -37,11 +38,44 @@ bool read_config(const char *server_name, DnsDb *db) {
     return 0;
 }
 
+
+
+bool parse_from_json(const char *json, DnsRequest *request)
+{
+ // Creating a json object *//*
+    json_object* parsed_json = json_tokener_parse(json);
+
+    json_object *tmp;
+    if (json_object_object_get_ex(parsed_json, "dns.flags.response", &tmp)) {
+        // Key exists
+        request->dns_flags_response = json_object_get_int(tmp);
+    }
+    if (json_object_object_get_ex(parsed_json, "dns.flags.recdesired", &tmp)) {
+        // Key exists
+        request->dns_flags_recdesired = json_object_get_int(tmp);
+    }
+
+    if (json_object_object_get_ex(parsed_json, "dns.qry.name", &tmp)) {
+        // Key exists
+        request->dns_qry_name = json_object_get_string(tmp);
+    }
+
+    if (json_object_object_get_ex(parsed_json, "dns.qry.type", &tmp)) {
+        // Key exists
+        request->dns_qry_type = json_object_get_int(tmp);
+    }
+
+    return 0;
+
+}
+
+
 int main(int argc, char **argv)
 {
     char *server_ip;
     char *server_name;
     DnsDb db;
+    DnsRequest request;
 
     if (argc != 3) {
         printf("command line parameter: server_ip name");
@@ -53,23 +87,41 @@ int main(int argc, char **argv)
     }
 
     printf("Server started: %s: %s\n", server_ip, server_name);
-	string hello = "ACK";
 
 	read_config(server_name, &db);
-	printf("%s", db.find_record("telematik")->ip.c_str());
+    // debug
+	// printf("%s\n", db.find_record("telematik")->ip.c_str());
 
     Udp udp(server_ip, UDPPORT);
     string recvmsg;   // received message
     string remaddr;   // remote address
 
+
     while(1)
    {
         udp.recv(recvmsg, remaddr);
         printf("Server: Received message \"%s\" from %s\n", recvmsg.c_str(), remaddr.c_str());
+        parse_from_json(recvmsg.c_str(), &request);
 
-		printf("Client : %s\n", recvmsg.c_str());
-        udp.send(hello, remaddr);
-		printf("Sent ACK.\n");
+       const DnsRecord *found = db.find_record(request.dns_qry_name);
+       if (found != NULL) {
+           if(found->ip.c_str() != NULL) {
+               printf("Client request: %s\n", recvmsg.c_str());
+               udp.send("FOUND!", remaddr);
+           }
+           else {
+               printf("Client request: %s\n", recvmsg.c_str());
+               udp.send("NO IP: REFUSED", remaddr);
+           }
+       }
+       else {
+           printf("Client request: %s\n", recvmsg.c_str());
+           udp.send("NO DNS RECORD: REFUSED!", remaddr);
+       }
+
+		// printf("Client : %s\n", recvmsg.c_str());
+        //udp.send(hello, remaddr);
+		//printf("Sent ACK.\n");
 	}
 
 	return 0;
---
+++ src/authorative_server.h
@@ -0,0 +1,20 @@
+//
+// Created by Oleksandra Baga on 29.11.20.
+//
+
+#ifndef TELEMATIC_PROJECTS_2021_AUTHORATIVE_SERVER_H
+#define TELEMATIC_PROJECTS_2021_AUTHORATIVE_SERVER_H
+
+#include <string>
+using namespace std;
+
+struct DnsRequest {
+    int dns_flags_response;
+    int dns_flags_recdesired;
+    string dns_qry_name;
+    int dns_qry_type;
+};
+
+typedef struct DnsRequest DnsRequest;
+
+#endif //TELEMATIC_PROJECTS_2021_AUTHORATIVE_SERVER_H
