0dc2c12f4e0f4607abe34982fa117abeff6f8d13
timeout <timeout@mi.fu-berlin.de>
2020-12-14 08:56:27 +0000

    Delete run_servers.sh

--- run_servers.sh
+++
@@ -1,8 +0,0 @@
-./recres dns.server.local &
-./authns ns &
-./authns ns.telematik &
-./authns ns.switch.telematik &
-./authns ns.router.telematik &
-./authns ns.fuberlin &
-./authns ns.homework.fuberlin &
-./authns ns.pcpools.fuberlin &
